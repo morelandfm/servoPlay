9aa913d9973046bd973d78a1d7ec89171d4db5b1
morelandfm <morelandfm50@gmail.com>
2026-02-02 13:25:26 -0800

    Update BTArmControlV1.ino
    Added print curSpeed to the serial output

--- BTArmControlV1.ino
+++ BTArmControlV1.ino
@@ -82,28 +82,27 @@ void onDisconnectedController(ControllerPtr ctl) {
 // ========= SEE CONTROLLER VALUES IN SERIAL MONITOR ========= //
 
 void dumpGamepad(ControllerPtr ctl) {
-  Serial.printf(
-  "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
-  /*"misc: 0x%02x, gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d\n"*/,
-  "curSpeed: \n"
-  ctl->index(),        // Controller Index
-  ctl->dpad(),         // D-pad
-  ctl->buttons(),      // bitmask of pressed buttons
-  ctl->axisX(),        // (-511 - 512) left X Axis
-  ctl->axisY(),        // (-511 - 512) left Y axis
-  ctl->axisRX(),       // (-511 - 512) right X axis
-  ctl->axisRY(),       // (-511 - 512) right Y axis
-  ctl->brake(),        // (0 - 1023): brake button
-  ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
-  ctl->miscButtons(),  // bitmask of pressed "misc" buttons
-  /*ctl->gyroX(),        // Gyro X
-  ctl->gyroY(),        // Gyro Y
-  ctl->gyroZ(),        // Gyro Z
-  ctl->accelX(),       // Accelerometer X
-  ctl->accelY(),       // Accelerometer Y
-  ctl->accelZ()*/        // Accelerometer Z
-  ctl->curSpeed(),
-  );
+    Serial.printf(
+        "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+        "misc: 0x%02x, gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d curSpeed: \n",
+        ctl->index(),        // Controller Index
+        ctl->dpad(),         // D-pad
+        ctl->buttons(),      // bitmask of pressed buttons
+        ctl->axisX(),        // (-511 - 512) left X Axis
+        ctl->axisY(),        // (-511 - 512) left Y axis
+        ctl->axisRX(),       // (-511 - 512) right X axis
+        ctl->axisRY(),       // (-511 - 512) right Y axis
+        ctl->brake(),        // (0 - 1023): brake button
+        ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+        ctl->miscButtons(),  // bitmask of pressed "misc" buttons
+        ctl->gyroX(),        // Gyro X
+        ctl->gyroY(),        // Gyro Y
+        ctl->gyroZ(),        // Gyro Z
+        ctl->accelX(),       // Accelerometer X
+        ctl->accelY(),       // Accelerometer Y
+        ctl->accelZ(),        // Accelerometer Z
+        curSpeed
+    );
 }
 
 //Imported function from previous code
